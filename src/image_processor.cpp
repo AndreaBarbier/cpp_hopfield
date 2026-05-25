8980af9191d81ec61ba592f991625dd5fdf055be
AndreaBarbier <barbierandrea2006@gmail.com>
2026-05-25 14:20:46 +0200

    fix(image_processor): fixed the logic of resizeImage function and renamed binarizePattern into colorsToBinary

--- src/image_processor.cpp
+++ src/image_processor.cpp
@@ -5,6 +5,32 @@
 #include <vector>
 
 namespace pf {
+// ###############################################################################
+// UTILITIES ON IMAGES
+// ###############################################################################
+sf::Image resizeImage(sf::Image const& img, sf::Vector2u targetSize) {
+  auto oldSize{img.getSize()};
+  assert(targetSize.x > oldSize.x && targetSize.y > oldSize.y &&
+         targetSize.x == 0u && targetSize.y == 0u);
+  if (targetSize.x > oldSize.x || targetSize.y > oldSize.y ||
+      targetSize.x == 0u || targetSize.y == 0u) {
+    throw std::runtime_error("Error: target image size is invalid.\n");
+  }
+  sf::Image newImg;
+  newImg.create(targetSize.x, targetSize.y);
+  for (auto j{0u}; j != targetSize.y; ++j) {
+    for (auto i{0u}; i != targetSize.x; ++i) {
+      auto j_o = (j * oldSize.y / targetSize.y);
+      auto i_o = (i * oldSize.x / targetSize.x);
+      newImg.setPixel(i, j, img.getPixel(i_o, j_o));
+    }
+  }
+  return newImg;
+}
+
+// ###############################################################################
+// IMAGES PROCESSING
+// ###############################################################################
 template <class T>
 struct Pattern {
   std::vector<T> data;
@@ -19,7 +45,7 @@ PatternRGB imageToColors(sf::Image const& img) {
   auto y{img.getSize().y};
   assert(x > 0u && y > 0u);
   if (x <= 0u || y <= 0u) {
-    throw std::runtime_error("Error: invalid image size\n");
+    throw std::runtime_error("Error: invalid image size.\n");
   }
   PatternRGB outPattern;
   for (auto j{0u}; j != img.getSize().y; ++j) {
@@ -30,35 +56,25 @@ PatternRGB imageToColors(sf::Image const& img) {
   return outPattern;
 }
 
-void resizePattern(PatternRGB& pattern, sf::Vector2u targetsize) {
-  if (targetsize.x > pattern.size.x || targetsize.y > pattern.size.y) {
-    throw std::runtime_error("Error: image size is too big.\n");
+PatternInt colorsToBinary(PatternRGB const& inPattern) {
+  assert(inPattern.data.size() > 0 && inPattern.size.x > 0 &&
+         inPattern.size.y > 0);
+  if (inPattern.data.size() == 0 || inPattern.size.x == 0 ||
+      inPattern.size.y == 0) {
+    throw std::runtime_error("Error: invalid input pattern.\n");
   }
-  std::vector<sf::Color> newdata;
-  for (auto j{0u}; j != targetsize.y; ++j) {
-    for (auto i{0u}; i != targetsize.x; ++i) {
-      auto j_o = j * (pattern.size.y / targetsize.y);
-      auto i_o = i * (pattern.size.x / targetsize.x);
-      newdata.push_back(pattern.data[j_o * pattern.size.x + i_o]);
-    }
-  }
-  pattern.data = std::move(newdata);
-  pattern.size = targetsize;
-}
-
-PatternInt binarizePattern(PatternRGB const& pattern) {
-  PatternInt result;
-  for (sf::Color const& c : pattern.data) {
-    int grigio = (static_cast<int>(c.r) + static_cast<int>(c.g) +
-                  static_cast<int>(c.b)) /
-                 3;
-    if (grigio > 127) {
-      result.data.push_back(1);
+  PatternInt outPattern;
+  for (sf::Color const& c : inPattern.data) {
+    int grey = (static_cast<int>(c.r) + static_cast<int>(c.g) +
+                static_cast<int>(c.b)) /
+               3;
+    if (grey > 127) {
+      outPattern.data.push_back(1);
     } else {
-      result.data.push_back(-1);
+      outPattern.data.push_back(-1);
     }
   }
-  result.size = pattern.size;
-  return result;
+  outPattern.size = inPattern.size;
+  return outPattern;
 }
 }  // namespace pf
