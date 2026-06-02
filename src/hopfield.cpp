2b71d016ab9766b257243d12ec727f75fc831695
AndreaBarbier <barbierandrea2006@gmail.com>
2026-06-03 00:37:12 +0200

    feat(hopfield): add matrix struct

---
+++ src/hopfield.cpp
@@ -0,0 +1,28 @@
+#include <SFML/Graphics.hpp>
+#include <algorithm>
+#include <cassert>
+#include <fstream>
+#include <string>
+#include <vector>
+
+namespace hopfield {
+// ============================================================
+// WEIGHT MATRIX
+// ============================================================
+
+/// @brief Matrix struct -- it represents the network's memory
+struct Matrix {
+  size_t rows;
+  size_t cols;
+  std::vector<double> data;
+
+  Matrix(size_t rows, size_t cols)
+      : rows{rows}, cols{cols}, data{std::vector<double>(rows * cols, 0.0)} {}
+
+  double& operator()(size_t i, size_t j) { return data[j * cols + i]; }
+  double const& operator()(size_t i, size_t j) const {
+    return data[j * cols + i];
+  }
+};
+
+}  // namespace hopfield
